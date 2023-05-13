192bcee9b5705278fc4933040aaa7dd860c923d0
Odysseys32 <xeon101110@gmail.com>
2023-05-13 16:23:19 +0600

    Version 0.1.3

--- src/main/java/plasma/controllers/ProfileController.java
+++ src/main/java/plasma/controllers/ProfileController.java
@@ -1,4 +1,22 @@
 package plasma.controllers;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import plasma.model.Profile;
+import plasma.service.ProfileService;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
 public class ProfileController {
+    private final ProfileService service;
+    @GetMapping("/manga/profile")
+    public String page(Model model){
+        List<Profile> profiles = service.findAll();
+        model.addAttribute("page", profiles);
+        return "/manga/profile";
+    }
 }
